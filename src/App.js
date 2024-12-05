af0c9e76af39deb574fc1f7cbe87150b75a1d36c
Jonas Svay <svay.jonas6@gmail.com>
2024-12-05 08:15:03 +0100

    chore: Tour

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Login from "./components/Login/Login.js"
 import Home from "./components/home/home";
+import Tour from "./components/tour/tour.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/tour" element={<Tour />} />
         </Routes>
       </div>
     </Router>
